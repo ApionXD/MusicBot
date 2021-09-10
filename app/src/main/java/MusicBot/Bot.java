6243eb5d131322d65d07b6e5cc3f73dc1003d195
Apion <masonbell09@gmail.com>
2021-09-09 20:30:44 -0400

    Add initialization of JDA instance

---
+++ app/src/main/java/MusicBot/Bot.java
@@ -0,0 +1,36 @@
+package MusicBot;
+
+import MusicBot.properties.PropManager;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+import java.util.Properties;
+
+@Slf4j @Getter
+
+public class Bot {
+    final Properties botProperties;
+    final String botToken;
+    final boolean setLight;
+    JDA jda;
+    public Bot() {
+        botProperties = PropManager.loadBotPropertyFile();
+        botToken = botProperties.getProperty("token");
+        setLight = Boolean.getBoolean(botProperties.getProperty("slowMode"));
+
+        try {
+            if (setLight) {
+                jda = JDABuilder.createLight(botToken).build();
+            }
+            else {
+                jda = JDABuilder.createDefault(botToken).build();
+            }
+        } catch (LoginException e) {
+            log.error("Token not valid! Please check your bot.properties file!");
+            System.exit(1);
+        }
+    }
+}
--- app/src/main/java/MusicBot/MusicBot.java
+++ app/src/main/java/MusicBot/MusicBot.java
@@ -1,16 +1,19 @@
 package MusicBot;
 
+import MusicBot.properties.BotProperties;
 import MusicBot.properties.PropManager;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 
+import javax.security.auth.login.LoginException;
 import java.io.IOException;
 import java.util.Properties;
 @Slf4j
 
 public class MusicBot {
     public static void main(String[] args) {
-        final Properties botProperties = PropManager.loadPropertyFile();
-
+        Bot musicBot = new Bot();
     }
 }
